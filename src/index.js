ee08315d83866e35cf1d0d1f4b6fb24d62511029
Yuriy Kravets <yucikkravets@gmail.com>
2018-12-16 15:45:23 +0100

    integrated with wc

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Button} from "@material/mwc-button";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
