dcfa6976842c2999fe5756594ae7738197e6b8fb
GeorgeTailor <yucikkravets@gmail.com>
2019-04-01 16:52:59 +0200

    Improved react integration example

--- src/App.js
+++ src/App.js
@@ -2,15 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <mwc-button>Click Here!</mwc-button>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			options: [
+				{
+					firstName: 'firstName',
+					lastName: 'lastName'
+				}
+			],
+			name: undefined,
+			date: undefined,
+			nameValid: true,
+			dateValid: true
+		};
+	}
+
+	submit() {
+		console.log(this.state.name);
+		console.log(this.state.date);
+		this.setState({
+			nameValid: this.state.name ? true : false,
+			dateValid: this.state.date ? true : false
+		}, () => {
+			console.log(this.state.nameValid);
+			console.log(this.state.dateValid);
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				<zoo-header imgsrc="assets/logo.png" headertext="Zoo web components in React"></zoo-header>
+				<form style={{maxWidth: 768 + 'px', margin: 0 + ' auto'}}>
+					<zoo-select inputerrormsg="Name is required" labeltext="Name" valid={this.state.nameValid ? 1 : ''}>
+						<select slot="selectelement">
+							{this.state.options.map(option => <option value={option.id} key={option}> { option.firstName } { option.lastName } </option>)}
+						</select>
+					</zoo-select>
+					<zoo-input labeltext="Input date field" inputerrormsg="Invalid value" infotext="Information text" valid={this.state.dateValid ? 1 : ''}>
+						<input type="date" placeholder="Placeholder" slot="inputelement" value={this.state.name}/>
+					</zoo-input>
+					<zoo-button class="submit-button" size="small" onClick={this.submit.bind(this)}>
+						<span slot="buttoncontent">Submit</span>
+					</zoo-button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Button} from "@material/mwc-button";
+import '@zooplus/zoo-web-components';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
